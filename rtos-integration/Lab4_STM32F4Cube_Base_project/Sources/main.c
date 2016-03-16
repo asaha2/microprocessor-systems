d26c8fd7e45283131395fe6fa1ef0155cfca5d2a
Aditya Saha <aditya.saha@mail.mcgill.ca>
2016-03-15 20:26:37 -0400

    Re-adjusted

--- rtos-integration/Lab4_STM32F4Cube_Base_project/Sources/main.c
+++ rtos-integration/Lab4_STM32F4Cube_Base_project/Sources/main.c
@@ -1,6 +1,6 @@
 /*******************************************************************************
   * @file    main.c
-  * @author  Aditya Saha & Habib Ahmed (original template by Ashraf Suyyagh)
+  * @author  Ashraf Suyyagh
 	* @version V1.2.0
   * @date    17-January-2016
   * @brief   This file demonstrates flasing one LED at an interval of one second
@@ -8,23 +8,9 @@
   ******************************************************************************
   */
 
-/* Includes */
 #include "stm32f4xx_hal.h"              // Keil::Device:STM32Cube HAL:Common
 #include "cmsis_os.h"                   // ARM::CMSIS:RTOS:Keil RTX
 #include "RTE_Components.h"             // Component selection
-
-#include "system_config.h"
-#include "kalman_filter.h"
-
-/* Extern declarations */
-TIM_Base_InitTypeDef TIM_Base_InitStruct;
-TIM_HandleTypeDef TIM_HandleStruct;
-ADC_InitTypeDef ADC1_Init;							
-ADC_ChannelConfTypeDef ADC1_Channel;
-ADC_HandleTypeDef ADC1_Handle;
-GPIO_InitTypeDef GPIOA_Init;						
-GPIO_InitTypeDef GPIOE_Init;						
-GPIO_InitTypeDef GPIOD_Init;
 
 extern void initializeLED_IO			(void);
 extern void start_Thread_LED			(void);
@@ -32,7 +18,7 @@ extern void Thread_LED(void const *argument);
 extern osThreadId tid_Thread_LED;
 
 /**
-	These lines are mandatory to make CMSIS-RTOS RTX work with the new Cube HAL
+	These lines are mandatory to make CMSIS-RTOS RTX work with te new Cube HAL
 */
 #ifdef RTE_CMSIS_RTOS_RTX
 extern uint32_t os_time;
@@ -41,6 +27,42 @@ uint32_t HAL_GetTick(void) {
   return os_time; 
 }
 #endif
+
+/**
+  * System Clock Configuration
+  */
+void SystemClock_Config(void) {
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+  /* Enable Power Control clock */
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  /* The voltage scaling allows optimizing the power consumption when the
+     device is clocked below the maximum system frequency (see datasheet). */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /* Enable HSE Oscillator and activate PLL with HSE as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 |
+                                RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
+}
 
 /**
   * Main function
@@ -52,9 +74,7 @@ int main (void) {
   HAL_Init();                               /* Initialize the HAL Library     */
 
   SystemClock_Config();                     /* Configure the System Clock     */
-	TIM3_Config();
-	ADC_Config();
-	
+
 	/* User codes goes here*/
   initializeLED_IO();                       /* Initialize LED GPIO Buttons    */
   start_Thread_LED();                       /* Create LED thread              */
